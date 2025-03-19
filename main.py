d4155d5c41685f3193c75f61cddf8d3120518342
mateusz29 <strzeleckimateusz10@gmail.com>
2025-03-19 23:21:51 +0100

    Add YOLOv8 object detection functionality and .gitignore file

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import gradio as gr
+from ultralytics import YOLO
+
+model = YOLO("models/YOLOv8l.pt")
+
+
+def detect_object(image):
+    results = model.predict(
+        source=image,
+        show_labels=True,
+        show_conf=True,
+    )
+
+    return results[0].plot() if results else None
+
+
 def main():
-    print("Hello from qgis-yolo-plugin!")
+    iface = gr.Interface(
+        fn=detect_object,
+        inputs=gr.Image(type="numpy", label="Upload Image"),
+        outputs=gr.Image(type="numpy", label="Result"),
+        title="YOLOv8 Object Detection",
+        description="Detect objects in images using YOLOv8.",
+    )
+
+    iface.launch()
 
 
 if __name__ == "__main__":
